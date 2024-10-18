3e4ad53fbc5d7dbc365f2288f4ca1ec92a166c16
Brendan Wood <wood.brendan.c@gmail.com>
2024-10-18 13:33:44 -0400

    ignore rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,3 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 import * as React from "react";
 import { useState, useEffect, useCallback } from "react";
 
